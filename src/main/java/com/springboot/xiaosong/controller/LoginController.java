56f9d3cfb1b9425b575de5382193f376dc60c3d6
Asong <Asong1107@163.com>
2019-06-29 12:42:18 +0800

    完善登录页面---2019.06.29

--- src/main/java/com/springboot/xiaosong/controller/LoginController.java
+++ src/main/java/com/springboot/xiaosong/controller/LoginController.java
@@ -4,6 +4,7 @@ package com.springboot.xiaosong.controller;
 import com.springboot.xiaosong.service.LoginService;
 import com.springboot.xiaosong.utils.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import javax.annotation.Resource;
 
 @RestController
 @Slf4j
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class LoginController {
 
     @Resource
--- src/main/java/com/springboot/xiaosong/service/impl/LoginServiceImpl.java
+++ src/main/java/com/springboot/xiaosong/service/impl/LoginServiceImpl.java
@@ -1,5 +1,7 @@
 package com.springboot.xiaosong.service.impl;
 
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import com.springboot.xiaosong.dao.LoginDao;
 import com.springboot.xiaosong.service.LoginService;
 import com.springboot.xiaosong.utils.Result;
@@ -7,21 +9,23 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Objects;
 
 
 @Service
 @Slf4j
 public class LoginServiceImpl implements LoginService {
-
     @Resource
     LoginDao loginDao;
     @Override
     public Result<String> userLogin(String json){
         log.info("接收到的数据："+json);
-//        String password = loginDao.userLogin(username);
-//        if(Objects.equals(password,psd)){
-//            return Result.success();
-//        }
+        JSONObject jsonObject = JSONUtil.parseObj(json);
+        String password = loginDao.userLogin(jsonObject.getStr("username"));
+
+        if(Objects.equals(password,jsonObject.getStr("password"))){
+            return Result.success("欢迎登陆");
+        }
         return Result.error("");
     }
 }
